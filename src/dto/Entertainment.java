f40ac0ebe85cafe07496802f6c534f64c13ff4ed
zzzmini <sm01079200887@gmail.com>
2025-07-21 10:19:42 +0900

    enter 작업 완료

--- src/dto/Entertainment.java
+++ src/dto/Entertainment.java
@@ -3,4 +3,20 @@ package dto;
 public class Entertainment {
     private int e_id;
     private String name;
+
+    public int getE_id() {
+        return e_id;
+    }
+
+    public void setE_id(int e_id) {
+        this.e_id = e_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/service/EnterService.java
+++ src/service/EnterService.java
@@ -1,8 +1,13 @@
 package service;
 
+import db.DBConn;
 import dto.Entertainment;
 import dto.GirlGroup;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,9 +15,29 @@ public class EnterService {
     private List<Entertainment> entertainments = new ArrayList<>();
     private List<GirlGroup> groups = new ArrayList<>();
 
+    public Connection conn = DBConn.getConnection();
+    public PreparedStatement psmt = null;
+    public ResultSet rs = null;
+    public String sql = null;
+
     public List<Entertainment> getAllEntertainment() {
         System.out.println("[EnterService.getAllEntertainment]");
-        return null;
+        try {
+            sql = "SELECT e_id, name FROM entertainment";
+            psmt = conn.prepareStatement(sql);
+            rs = psmt.executeQuery();
+
+            // 순회하면서 리스트에 담기
+            while (rs.next()) {
+                Entertainment entertainment = new Entertainment();
+                entertainment.setE_id(rs.getInt("e_id"));
+                entertainment.setName(rs.getString("name"));
+                entertainments.add(entertainment);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return entertainments;
     }
 
     public List<GirlGroup> getGroups(int enterId) {
